8af05ba5e866c3589f159f9710bd765e6b3f4927
Xuehai Qiao <xuehaiqiao@gmail.com>
2023-07-03 17:17:39 -0400

    fixed asyic problem

--- HWs/my-next-app/src/app/page.tsx
+++ HWs/my-next-app/src/app/page.tsx
@@ -3,44 +3,62 @@ import { Welcome } from '@/types';
 import { Pagination } from '@mui/material';
 import Head from 'next/head';
 import Image from 'next/image'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default async function Page() {
-  const [page, setPage] = React.useState(1);
-  console.log("rerender");
-  const data = await getData(page);
-  
+export default function Page() {
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState<Welcome | null>(null);
+
+  const fetchData = async () => {
+    setLoading(true);
+    const result = await getData(page);
+    console.log(result)
+    setData(result);
+    setLoading(false);
+    console.log(loading);
+  };
+
+  useEffect(() => {
+    console.log("reload1");
+    fetchData();
+  }, [page]);
+
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
   };
 
-  return (
+  return <> {loading ? <div>Loading...</div> :
     <div>
       <Head>
         <title>Create Next App</title>
       </Head>
       <div className='character-items'>
-      {data.results.map(character => {
-        return <div className='character' key={character.id}>
-          {character.name}
-          <Image
-            src={character.image}
-            alt={character.name}
-            width="200"
-            height="200"
-          />
-        </div>
-      })}
+        {data?.results.map(character => {
+          return <div className='character' key={character.id}>
+            {character.name}
+            <Image
+              src={character.image}
+              alt={character.name}
+              width="200"
+              height="200"
+            />
+          </div>
+        })}
       </div>
-
 
       <Pagination
         onChange={handleChange}
-        count={data.info.pages}
+        count={data ? data.info.pages : 1}
         page={page}
         color="primary" />
     </div>
-  )
+
+  }
+  </>
+
+
+
 }
 
 async function getData(pageNum: number): Promise<Welcome> {
